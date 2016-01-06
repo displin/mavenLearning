27b3bb31f965b2f0f5457255e5dd5d5e141e3ace
displin <displin@126.com>
2016-01-06 23:55:47 +0800

    引入log4j记录日志
    1. web.xml新增监听器、properties路径、刷新频率；
    2.pom.xml新增log4j依赖
    3.新增log4j.properties

--- maven05/maven05-webapp/src/main/java/com/lin/maven05_webapp/helloworld/controller/HelloWorldController.java
+++ maven05/maven05-webapp/src/main/java/com/lin/maven05_webapp/helloworld/controller/HelloWorldController.java
@@ -1,14 +1,22 @@
 package com.lin.maven05_webapp.helloworld.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
+	public static Logger logger = Logger.getLogger(HelloWorldController.class);
 	
 	@RequestMapping("/sayHelloWorld")
 	public String sayHelloWorld() {
 		System.out.println("Hello World!");
+		
+		logger.error("log4j --- error");
+		logger.warn("log4j --- warn");
+		logger.info("log4j --- info");
+		logger.debug("log4j --- debug");
+		
 		return "success";
 	}
 }
